f4e253b7259ad0f65d9a7daf8cbd3080a2e13e47
Kawann <kawannmarcelo7@gmail.com>
2025-03-18 23:04:32 -0300

    feat: making details in the project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { ProtectedRoute } from './routes/ProtectedRoute';
+import Home from './pages/Home';
+import LoginRegister from './pages/LoginRegister';
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LoginRegister />} />
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
