c4ceea6fb135ce289b25fb6e8d15cff8dea54f20
Kawann <kawannmarcelo7@gmail.com>
2025-03-18 23:01:47 -0300

    feat: adding validation popup and axios for api requests

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,52 @@
 import { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useAuth } from '../context/AuthContext';
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const url = "https://form-trilhas-api.onrender.com";
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+    
+        setLoading(true);
+        setErrorMessage('');
+    
+        if (!email || !password) {
+          setErrorMessage('Todos os campos precisam ser preenchidos!');
+          toast.error('Todos os campos precisam ser preenchidos!');
+          setLoading(false);
+          return;
+        }
+    
+        try {
+          const response = await axios.post(`${url}/users/login`, { email, password });
+    
+          if (response.status === 200) {
+            login(response.data.token);
+            toast.success('Login realizado com sucesso');
+            setTimeout(() => {
+              navigate('/home');
+            }, 500);
+          }
+        } catch (error) {
+          const message = error.response?.data?.message || 'Erro ao tentar fazer login.';
+          setErrorMessage(message);
+          toast.error(message);
+        } finally {
+          setLoading(false);
+        }
+      };
+
     return (
         <div className="flex flex-col justify-center items-center w-full p-4 gap-6">
             <div className="relative">
@@ -26,11 +70,17 @@ export default function Login() {
                 <i className="fa-solid fa-lock absolute top-1/2 -translate-y-1/2 right-3 text-gray-700"></i>
             </div>
 
-            <button 
-                className="p-4 w-65 cursor-pointer rounded-2xl bg-blue-400 font-semibold text-white"
+            <button
+                className={`p-4 w-65 cursor-pointer rounded-2xl font-semibold text-white ${
+                    loading ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-400'
+                }`}
+                onClick={handleLogin}
+                disabled={loading}
             >
-                ENTRAR
+            {loading ? 'ENTRANDO...' : 'ENTRAR'}
             </button>
+
+            <ToastContainer />
         </div>
-    )
+    );
 }
