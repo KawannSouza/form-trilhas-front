ac6322e1ccd1342abab69274801c09ce81ad2fd2
Kawann <kawannmarcelo7@gmail.com>
2025-03-18 22:29:18 -0300

    feat: adding the additional information modal to the registration page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,16 @@
 //IMPORTAÇÕES
 import { useEffect, useState } from 'react';
+import Modal from '../components/Modal';
+import FileUpload from '../components/FileUpload';
 
 //FUNÇÃO QUE GERA A PÁGINA DE REGISTRO
 export default function Register() {
+    const [openInfosModal, setOpenInfosModal] = useState(false);
+    const [openFileModal, setOpenFileModal] = useState(false);
+    const handleFileSelect = (file) => {
+        console.log('Arquivo selecionado:', file);
+    };
+
     //ESTADOS PARA ARMAZENAR OS VALORES DO FORMULÁRIO
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -70,14 +78,66 @@ export default function Register() {
             <div className="flex flex-col gap-4 lg:flex-row">
                 <button
                     className="bg-gray-700 text-white font-semibold py-2 px-2 w-60 rounded-xl cursor-pointer whitespace-nowrap lg:[py-2 px-6 w-70]"
+                    onClick={() => setOpenInfosModal(true)}
                 >
                     + Informações Adicionais</button>
                 <button
                     className="bg-gray-700 text-white font-semibold py-2 px-2 w-60 rounded-xl cursor-pointer whitespace-nowrap lg:[py-2 px-6 w-70]"
-                    onClick={() => console.log(name)}
+                    onClick={() => setOpenFileModal(true)}
                 >
                     Fazer Upload do Currículo</button>
             </div>
+
+            <Modal isOpen={openInfosModal}>
+                <div className="">
+                    <h2 className="text-gray-700 text-3xl font-bold mb-10">Informações Adicionais</h2>
+                    <div className="flex flex-col justify-center items-center gap-6">
+                        <div className="relative">
+                            <input 
+                                type="text" 
+                                placeholder="Digite seu CPF"
+                                value={cpf}
+                                onChange={(e) => setCpf(e.target.value)}
+                                className="p-4 rounded-2xl border-2 border-blue-400 outline-0"
+                            />
+                            <i className="fa-solid fa-key absolute top-1/2 -translate-y-1/2 right-3 text-gray-700"></i>
+                        </div>
+                        <div className="relative">
+                            <input 
+                                type="text" 
+                                placeholder="Digite seu CEP" 
+                                value={cep}
+                                onChange={(e) => setCep(e.target.value)}
+                                className="p-4 rounded-2xl border-2 border-blue-400 outline-0"
+                            />
+                            <i className="fa-solid fa-inbox absolute top-1/2 -translate-y-1/2 right-3 text-gray-700"></i>
+                        </div>
+                        <div className="relative">
+                            <input 
+                                type="text"
+                                placeholder="Estado" 
+                                value={uf}
+                                onChange={(e) => setUf(e.target.value)}
+                                className="p-4 rounded-2xl border-2 border-blue-400 outline-0"
+                            />
+                            <i className="fa-solid fa-thumbtack absolute top-1/2 -translate-y-1/2 right-3 text-gray-700"></i>
+                        </div>
+                        <div className="relative">
+                            <input 
+                                type="text" 
+                                placeholder="Logradouro"
+                                value={logradouro}
+                                onChange={(e) => setLogradouro(e.target.value)}
+                                className="p-4 rounded-2xl border-2 border-blue-400 outline-0"
+                            />
+                            <i className="fa-solid fa-location-dot absolute top-1/2 -translate-y-1/2 right-3 text-gray-700"></i>
+                        </div>
+                    </div>
+                    <div className="flex justify-end mt-10">
+                        <button className="bg-red-600 text-white py-1 px-4 rounded-md cursor-pointer" onClick={() => setOpenInfosModal(false)}>FECHAR</button>
+                    </div>
+                </div>
+            </Modal>
             
             <button
                 className="bg-blue-400 text-white font-semibold py-2 px-2 w-60 rounded-xl cursor-pointer lg:[py-2 px-6 w-70]"
